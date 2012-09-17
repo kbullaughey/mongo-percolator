0bc93ee74f694fb72baf351b392143ff12a8a30d
Kevin Bullaughey <kbullaughey@gmail.com>
2012-09-17 12:15:13 -0400

    now requiringing operation_definition and mongo_mapper

--- lib/mongo_percolator.rb
+++ lib/mongo_percolator.rb
@@ -1,7 +1,10 @@
-require "mongo_percolator/version"
+require 'mongo_mapper'
+require 'mongo_percolator/version'
 
 module MongoPercolator
   def self.whoami?
-    "I am the (Mongo) Percolator!"
+    'I am the (Mongo) Percolator!'
   end
 end
+
+require 'mongo_percolator/operation_definition'
