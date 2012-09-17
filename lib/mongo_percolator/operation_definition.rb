2ecd39a3831711221c72a06450d86ac0397bdeba
Kevin Bullaughey <kbullaughey@gmail.com>
2012-09-17 15:58:28 -0400

    operations compute the values of computed properties on nodes

--- lib/mongo_percolator/operation_definition.rb
+++ lib/mongo_percolator/operation_definition.rb
@@ -5,27 +5,51 @@ module MongoPercolator
   # Each operation should have an emit function
   class OperationDefinition
     include MongoMapper::Document
+    include Addressable
     set_collection_name "operations_graph"
 
     # These domain-specific langauge methods are to be used in specifying the 
     # operation definition that inherits from this class.
     module DSL
-      # Adds a 
-      def updates(property)
-        @updated_properties ||= []
-        @updated_properties.push property.to_s
+      # Adds a computed property. It takes a block, which should define a property
+      # by the same name using any of the standard MongoMapper means, such as key,
+      # one, many, etc.
+      def computes(property)
+        @computed_properties ||= []
+        @computed_properties.push property.to_s
+        yield
+      end
+
+      # Adds a dependency
+      # @param path [String] Dot-separated string giving the location of data 
+      #   needed for emit()
+      def depends_on(path)
+        @dependencies ||= Set.new
+        @dependencies.add path
       end
     end
 
     # These class methods are for general use and not really part of the DSL
     module ClassMethods
-      # Return the list of properties that are updated by this operation
-      def updated_properties
-        @updated_properties || []
+      # Return the list of properties that are computed by this operation
+      def computed_properties
+        @computed_properties || []
+      end
+
+      def dependencies
+        @dependencies || []
       end
     end
 
     extend DSL    
     extend ClassMethods
+
+    # Collect all the data required to perform the operation. This instance
+    # method requires that all the parent associations on which computed 
+    # properties depend are set and available on the instance.
+    # @return [Hash] data passed to emit
+    def gather(root)
+      Hash[self.class.dependencies.collect { |dep| [dep, fetch(dep, root)] }]
+    end
   end 
 end
---
+++ spec/unit/operation_definition_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe "MongoPercolator::OperationDefinition unit" do
+  before :all do
+    # Set up a derived class with no computed properties
+    class NoOp < MongoPercolator::OperationDefinition
+    end
+
+    class Animals
+      def wild
+        ["sloth", "binturong"]
+      end
+      def farm
+        ["pig"]
+      end
+    end
+
+    # Set up a derived class with a few computed properties
+    class RealOp < MongoPercolator::OperationDefinition
+      def emit(inputs)
+      end
+      key :animals, Animals
+      computes(:pets) { key :pets, Array }
+      depends_on 'animals.farm'
+      depends_on 'animals.wild'
+    end
+  end
+
+  describe "NoOp" do
+    it "shouldn't have any computed properties" do
+      NoOp.computed_properties.should == []
+    end
+  end
+
+  describe "RealOp" do
+    it "should have pets as a computed property" do
+      RealOp.computed_properties.should include('pets')
+    end
+
+    it "can gather the data for the operation" do
+      node = double(:animals => Animals.new)
+      data = RealOp.new.gather node
+      data.should be_kind_of(Hash)
+      data['animals.farm'].should == ["pig"]
+      data['animals.wild'].should == ["sloth", "binturong"]
+    end
+  end
+end
