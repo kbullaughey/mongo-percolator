24b51baa68e9c47533e2faf80aee44d0726da1f5
Kevin Bullaughey <kbullaughey@gmail.com>
2012-09-19 17:15:45 -0400

    more helpers for dealing with addresses

--- lib/mongo_percolator/addressable.rb
+++ lib/mongo_percolator/addressable.rb
@@ -1,8 +1,55 @@
 module MongoPercolator
+  # Addressalbe can be used either as a standalone module or mixed into a class.
   module Addressable
-    # Get data assuming that the dot-separated path is a valid method chain
-    def fetch(path, target = self)
-      pieces = path.split "."
+
+    # Use Forwardable to create instance versions that pass through to the 
+    # corresponding class methods
+    extend Forwardable
+    def_delegators :Addressable, :match_head, :head, :tail
+
+    #---------------------------
+    # Instance methods for mixin
+    #---------------------------
+
+    def changed?(addr, target=self)
+      Addressable.changed? addr, target
+    end
+
+    def fetch(addr, target=self)
+      Addressable.fetch addr, target
+    end
+
+    #---------------------------------
+    # Class methods for standalone use
+    #---------------------------------
+
+    # Indicate whether data at an address has changed, assumes that target
+    # implements the requisite _changed? functions. This method checks each
+    # layer in the address to see if it's changed.
+    # @raises [NameError] When a requisite _changed method is not implemented.
+    def self.changed?(addr, target)
+    end
+
+    # Return a proc that can be used to match head on the front of a address.
+    def self.match_head(h)
+      Proc.new {|addr| head(addr) == h }
+    end
+
+    # Return the first piece of the address.
+    def self.head(addr)
+      addr.split(".").first
+    end
+
+    # Return everything but the head
+    def self.tail(addr)
+      pieces = addr.split(".")
+      pieces.shift
+      pieces.join "."
+    end
+
+    # Get data assuming that the dot-separated address is a valid method chain
+    def self.fetch(addr, target)
+      pieces = addr.split "."
       while !pieces.empty?
         target = target.send pieces.shift
       end
--- spec/unit/addressable_spec.rb
+++ spec/unit/addressable_spec.rb
@@ -24,8 +24,24 @@ describe "MongoPercolator::Addressable unit" do
     Layer1.new.fetch('bing').should be_kind_of(Layer1::Layer2)
   end
 
-  it "can address a multi-layer path" do
+  it "can address a multi-layer address" do
     Layer1.new.fetch('bing.bam.boom').should == "Ouch!"
+  end
+
+  describe "match_head" do
+    before :each do
+      @addrs = ['fish.red_snapper', 'fish.bass', 'beer.bass', 'beer.yingling']
+    end
+
+    it "can use match_head to filter addresses" do
+      matching = @addrs.select &MongoPercolator::Addressable.match_head('fish')
+      matching.should == ['fish.red_snapper', 'fish.bass']
+    end
+  
+    it "can use match_head as an instance function" do
+      matching = @addrs.select &Layer1.new.match_head('fish')
+      matching.should == ['fish.red_snapper', 'fish.bass']
+    end
   end
 end
 
