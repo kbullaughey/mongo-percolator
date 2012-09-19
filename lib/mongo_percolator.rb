9a4f5ff3bcc53cb7f8cad7f61f79f04227c24122
Kevin Bullaughey <kbullaughey@gmail.com>
2012-09-19 17:13:45 -0400

    added file to contain exception classes

--- lib/mongo_percolator.rb
+++ lib/mongo_percolator.rb
@@ -5,8 +5,17 @@ module MongoPercolator
   def self.whoami?
     'I am the (Mongo) Percolator!'
   end
+
+  def self.connect(db = "mongo_percolator_test")
+    options = {}
+    options[:safe] ||= {:w => 1}
+    MongoMapper.connection = Mongo::Connection.new("localhost", nil, options)
+    MongoMapper.database = db
+    raise RuntimeError, "Failed to connect to MongoDB" if MongoMapper.connection.nil?
+  end
 end
 
 require 'mongo_percolator/addressable'
+require 'mongo_percolator/exceptions'
 require 'mongo_percolator/node'
 require 'mongo_percolator/operation_definition'
---
+++ lib/mongo_percolator/exceptions.rb
@@ -0,0 +1,5 @@
+module MongoPercolator
+  # Used when something is missing from the mongo database
+  class MissingData < StandardError; end
+  class Collision < StandardError; end
+end
