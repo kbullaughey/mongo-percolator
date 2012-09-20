8bd110e138383a0db27495250bb1d1ec57e6a3e5
Kevin Bullaughey <kbullaughey@gmail.com>
2012-09-20 17:49:55 -0400

    added function to duplicate a hash without mongo _id properties

--- lib/mongo_percolator.rb
+++ lib/mongo_percolator.rb
@@ -11,11 +11,25 @@ module MongoPercolator
     options[:safe] ||= {:w => 1}
     MongoMapper.connection = Mongo::Connection.new("localhost", nil, options)
     MongoMapper.database = db
-    raise RuntimeError, "Failed to connect to MongoDB" if MongoMapper.connection.nil?
+    raise RuntimeError, "Failed to connect to MongoDB" if 
+      MongoMapper.connection.nil?
+  end
+
+  # Duplicate a hash, but remove all the '_id' keys, recursively. Be careful 
+  # about cycles.
+  def self.dup_hash_without_ids(x)
+    if x.kind_of? Hash
+      x = Hash[x.each.collect {|k,v| [k, dup_hash_without_ids(v)] }]
+      x.delete '_id'
+    elsif x.kind_of? Array
+      x = x.collect {|v| dup_hash_without_ids(v) }
+    end
+    x
   end
 end
 
 require 'mongo_percolator/addressable'
+require 'mongo_percolator/addressable/diff'
 require 'mongo_percolator/exceptions'
 require 'mongo_percolator/node'
 require 'mongo_percolator/operation_definition'
