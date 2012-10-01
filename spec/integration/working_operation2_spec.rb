3295a430f75558cd4bb1780bfe9fa556f3fccc7e
Kevin Bullaughey <kbullaughey@gmail.com>
2012-09-30 21:53:37 -0400

    added another spec for summation test

--- spec/integration/working_operation2_spec.rb
+++ spec/integration/working_operation2_spec.rb
@@ -39,35 +39,43 @@ describe "MongoPercolator Node & Operation integration (2)" do
       @node.sum.should == 0
     end
 
-    it "can compute a sum" do
-      @node.compute.sum_terms = [SumTerm.new(:value => 1.5), SumTerm.new(:value => 0.5)]
-      @node.compute.recompute!
-      @node.reload
-      @node.sum.should == 2.0
-    end
+    context "Two initial terms" do
+      before :each do
+        @node.compute.sum_terms = [SumTerm.new(:value => 0.5), SumTerm.new(:value => 0.5)]
+        @node.compute.recompute!
+        @node.reload
+      end
 
-    it "is not marked as old if nothing has changed when saved" do
-      @node.compute.sum_terms = [SumTerm.new(:value => 0.5), SumTerm.new(:value => 0.5)]
-      @node.compute.recompute!
-      @node.reload
-      @node.sum.should == 1.0
-      @node.compute._old.should be_false
-      @node.compute.save.should be_true
-      @node.reload
-      @node.compute._old.should be_false
-    end
-
-    it "is updated when terms are added" do
-      @node.compute.sum_terms = [SumTerm.new(:value => 0.5), SumTerm.new(:value => 0.5)]
-      @node.compute.recompute!
-      @node.reload
-      @node.sum.should == 1.0
-      new_term = SumTerm.create(:value => 9.9)
-      @node.compute.sum_term_ids << new_term.id
-      @node.compute.save.should be_true
-      MongoPercolator.percolate
-      @node.reload
-      @node.sum.should == 10.9
+      it "can compute a sum" do
+        @node.sum.should == 1.0
+      end
+  
+      it "is not marked as old if nothing has changed when saved" do
+        @node.compute._old.should be_false
+        @node.compute.save.should be_true
+        @node.reload
+        @node.compute._old.should be_false
+      end
+  
+      it "is updated when terms are added" do
+        new_term = SumTerm.create(:value => 9.9)
+        @node.compute.sum_term_ids << new_term.id
+        @node.compute.save.should be_true
+        MongoPercolator.percolate
+        @node.reload
+        @node.sum.should == 10.9
+      end
+  
+      it "is updated when a term changes value" do
+        terms = @node.compute.sum_terms
+        terms[0].value = 3.99
+        terms[0].save.should be_true
+        @node.compute.reload
+        @node.compute.old?.should be_true
+        MongoPercolator.percolate
+        @node.reload
+        @node.sum.should == 4.49
+      end
     end
   end
 end
